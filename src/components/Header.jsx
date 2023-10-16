dd5245d36c61f33bdbec51b5a8f03d871597db69
Ryan Mitchell <ryanm4398@gmail.com>
2023-10-15 23:01:41 -0400

    added nav effects and border effect for the Avatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,21 +6,26 @@ import WhatshotIcon from '@mui/icons-material/Whatshot';
 import RyanFlowerAvi from '../assets/images/flower.jpg';
 import DesignServicesIcon from '@mui/icons-material/DesignServices';
 import ContactPageIcon from '@mui/icons-material/ContactPage';
+import { useState } from 'react';
 
 const navItems = [
     {
+        id: 1,
         name: "Home",
         path: "/",
         icon: <HomeIcon sx={{ mr: 0.5 }} fontSize="medium" />
     }, {
+        id: 2,  
         name: "About Me",
         path: "/about-me",
         icon: <WhatshotIcon sx={{ mr: 0.5 }} fontSize="medium" />
     }, {
+        id: 3,
         name: "Projects",
         path: "/projects",
         icon: <DesignServicesIcon sx={{ mr: 0.5 }} fontSize="medium" />
     }, {
+        id: 4,
         name: "Contact",
         path: "/contact-me",
         icon: <ContactPageIcon sx={{ mr: 0.5 }} fontSize="medium" />
@@ -28,28 +33,32 @@ const navItems = [
 ];
 
 const Header = () => {
+    const [active, setActive] = useState(undefined);
+    const handleLinkClick = (id) => {
+        setActive(id);
+    };
+
     return (
         <>
             <header>
-                <Grid container style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}} className="headerContainer">
+                <Grid container style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }} className="headerContainer">
                     <Grid item className="headerTitle">
-                        <Avatar alt="Ryan Pic" style={{border: 'dotted thick white'}} src={RyanFlowerAvi} sx={{ width: 100, height: 100 }} />
+                        <Avatar alt="Ryan Pic" className='aviPic' src={RyanFlowerAvi} sx={{ width: 100, height: 100 }} />
                         <h1>Ry.Mits</h1>
                     </Grid>
-                    <Grid item style={{justifyContent: 'right', marginTop: '5px'}} className='navbar'>
-                        <nav className='navbar' role="presentation">
-                            <Breadcrumbs aria-label="breadcrumb">
-                                {navItems.map((item, index) => (
-                                    <Link
-                                        key={index}
-                                        style={{ display: 'flex', alignItems: 'center', color: 'white', textDecoration: 'none' }}
-                                        to={item.path}
-                                    >
-                                        {item.icon}
-                                        {item.name}
-                                    </Link>
-                                ))}
-                            </Breadcrumbs>
+                    <Grid item style={{ justifyContent: 'right', marginTop: '5px' }}>
+                        <nav className='navbar'>
+                            {navItems.map((item, index) => (
+                                <Link
+                                    key={index}
+                                    to={item.path}
+                                    className={item.id === active ? 'navItemActive' : 'navItem'}
+                                    onClick={() => handleLinkClick(item.id)}
+                                >
+                                    {item.icon}
+                                    {item.name}
+                                </Link>
+                            ))}
                         </nav>
                     </Grid>
                 </Grid>
