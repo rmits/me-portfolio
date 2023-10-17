be15282a10f5d4aac8137fb55d5708adc1957615
Ryan Mitchell <ryanm4398@gmail.com>
2023-10-16 21:24:07 -0400

    fixed the projects tab alignment issues

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import scrummyFish from '../assets/images/scrummy-fish.png';
 import catDog from '../assets/images/cat-dog.jpeg';
 import thoughtBubble from '../assets/images/thought-bubble.png';
+import gameChipLogo from '../assets/images/game-logo.png';
 
 const projectData = [
     {
@@ -10,6 +11,11 @@ const projectData = [
         href: 'https://rmits.github.io/scrummy-food-recipe-app/',
         image: <img src={scrummyFish} alt="Scrummy Fish" width={'10%'}></img>,
         borderColor: 'blue'
+    }, {
+        name: <span><h1>GameChip:</h1>Ever been stuck on that annoying level? Not anymore with Gamechip, where gamers ask questions and solve them as well!</span>,
+        href: 'https://radiant-everglades-30632-b3c22f102a99.herokuapp.com/',
+        image: <img src={gameChipLogo} alt="GameChip Logo" width={'10%'}></img>,
+        borderColor: 'pink'
     }, {
         name: <span><h1>ThoughtFull API (Back-End Only):</h1>The start of an application to have users, thoughts that they can create, and reactions to other users thoughts!</span>,
         href: 'https://drive.google.com/file/d/1YvA5iwEqM-y9Ikl9NeWpP-yOHNgMJhq7/view',
@@ -29,12 +35,15 @@ const Projects = () => {
         <>
             <Grid container style={{display: 'flex', flexDirection: 'column'}}>
                 {projectData.map((projects, index) => (
-                    <Grid key={index} item style={{display: 'flex', padding: '1%', marginTop: '2%', marginBottom: '2%', flexDirection: 'row', color: 'white', alignItems:'center', textAlign: 'left', justifyContent: 'space-between', backgroundColor: 'gray', border: `outset ${projects.borderColor}`, fontSize: '20px', fontWeight: 'bold'}}>
-                        {projects.image}
-                        {projects.name}
-                        <IconButton aria-label="linkButton" color='error' href={projects.href}>
-                            <ExitToAppIcon />
-                        </IconButton>
+                    <Grid container key={index} style={{display: 'flex', padding: '1%', marginTop: '2%', marginBottom: '2%', flexDirection: 'row', color: 'white', textAlign: 'left', justifyContent: 'space-between', backgroundColor: 'gray', border: `outset ${projects.borderColor}`, fontSize: '20px', fontWeight: 'bold'}}>
+                        <Grid item>
+                            {projects.image}{projects.name}
+                        </Grid>
+                        <Grid item style={{alignSelf: 'center'}}>
+                            <IconButton aria-label="linkButton" color='error' href={projects.href}>
+                                <ExitToAppIcon />
+                            </IconButton>
+                        </Grid>
                     </Grid> 
                 ))}
             </Grid>
